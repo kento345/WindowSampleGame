c11bd99688541bd54576e46890216cac9538aa48
kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2026-01-21 12:27:22 +0900

    BulltConstantBuffrが複製されず配列の一番最後にしかない

--- object/BulletManager.cpp
+++ object/BulletManager.cpp
@@ -1,6 +1,6 @@
 #include "BulletManager.h"
 
-
+//ConstantBuffrの配列の4にしか作られていない(複製されてない)
 void BulletManager::createConstant(constant_buffer & bulletConst, device& deviceInstance, descriptor_heap& constantBufferDescriptorInstance_) noexcept {
 	if (!bulletConst.create(deviceInstance, constantBufferDescriptorInstance_, sizeof(bullet::ConstBufferData), 3)) {
 		assert(false && "バレット用コンスタントバッファの作成に失敗しました");
@@ -15,10 +15,10 @@ void BulletManager::createBullet(bullet& bulletInstance, constant_buffer* bullet
 		bulletInstance.color()
 	};
 	UINT8* pBulletData{};
-	bulletConstantInstant[i].constanceBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&pBulletData));
+	bulletConstantInstant[3].constanceBuffer()->Map(0, nullptr, reinterpret_cast<void**>(&pBulletData));
 	memcpy_s(pBulletData, sizeof(bulletData), &bulletData, sizeof(bulletData));
-	bulletConstantInstant[i].constanceBuffer()->Unmap(0, nullptr);
-	commandlistInstance.get()->SetGraphicsRootDescriptorTable(1, bulletConstantInstant[i].getGpuDescriptorHandle());
+	bulletConstantInstant[3].constanceBuffer()->Unmap(0, nullptr);
+	commandlistInstance.get()->SetGraphicsRootDescriptorTable(1, bulletConstantInstant[3].getGpuDescriptorHandle());
 
 	bulletPolygonInstance.draw(commandlistInstance);
 
