db88c276b0b75f353b9d331518b7fbd0597969cf
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2026-02-13 16:24:40 +0900

    no message

--- object/bullet.h
+++ object/bullet.h
@@ -22,11 +22,12 @@ public:
 
 	DirectX::XMFLOAT4 color()const noexcept;
 
+	DirectX::XMFLOAT3 startPos_{};
+
 private:
 	DirectX::XMMATRIX world_ = DirectX::XMMatrixIdentity();
 	DirectX::XMFLOAT4 color_ = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 
-	DirectX::XMFLOAT3 startPos_{};
 	float move_ = 0.0f;
 };
 
